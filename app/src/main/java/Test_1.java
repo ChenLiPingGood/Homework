01ef2eddeb29f96766b3ba63b42ad2614e59f881
1507135840@qq.com <1507135840@qq.com>
2020-04-24 21:35:51 +0800

    B modify Test_2

--- app/src/main/java/Test_1.java
+++ app/src/main/java/Test_1.java
@@ -1,5 +1,8 @@
 public class Test_1 {
     public static void main(String argc[]){
-        System.out.println("Hello,Welcome to here!");
+
+        System.out.println("A:  Hello,Welcome to here!");
+        //第一次修改
+        System.out.println("B:   Ok,Thanks!");
     }
 }
